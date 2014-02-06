cfd22bdb8e585e746ca04e8f00ffbefdbb4542c2
Mike Adeleke <madeleke13@gmail.com>
2014-02-06 16:25:44 -0600

    added test for add feature

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -1,5 +1,10 @@
-require "test/unit"
+require 'test/unit'
+require "app/calculator"
 
 class CalculatorTest < Test::Unit::TestCase
-
+  def test_can_add
+    calculator = Calculator.new
+    actual = calculator.add(2, 2)
+    assert_equal(4, actual)
+  end
 end
